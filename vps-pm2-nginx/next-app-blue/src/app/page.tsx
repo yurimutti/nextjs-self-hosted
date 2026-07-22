570e7e6830d8cae9f1fab328915bd26ab3e04a26
Yuri Mutti <muttiyuri@gmail.com>
2026-07-22 19:45:19 +0200

    test: blue v2 marker for devopness webhook test

--- vps-pm2-nginx/next-app-blue/src/app/page.tsx
+++ vps-pm2-nginx/next-app-blue/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-          Next App <span className="text-blue-400">Blue</span>
+          Next App <span className="text-blue-400">Blue V2 DEVOPNESS</span>
         </h1>
       </main>
     </div>
